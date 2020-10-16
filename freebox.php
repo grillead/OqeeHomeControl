27f50bc553e7c1bc546b117d2d821d11d00b4e4a
grillead <adrien.grillet@gmail.com>
2020-10-16 13:51:31 +0200

    Update freebox.php
    Modif pour preparation de le reconnaissance du numeros de chaine.

--- freebox.php
+++ freebox.php
@@ -1,6 +1,10 @@
 <?php
+//include 'fonction.php';
+
 $setDevice="e-infor.fr";
-$setPort="1123"; 
+$setPort="1123";
+$conn = mysql_connect("localhost:3306", "root", ""); 
+$nom = isset($_GET['nom']) ? $_GET['nom'] : null ;
 
 shell_exec("adb connect ".$setDevice.":".$setPort." ");
 sleep(1);
@@ -8,15 +12,27 @@ sleep(1);
 if ($_GET["cmd"] == "mycanal") 
 {
 shell_exec('adb shell monkey -p com.canal.android.canal 1');
+break;
 }
+
+if ($nom != NULL) 
+{
+$nom = $_GET["nom"];
+$sqlchaine = " SELECT free FROM `chaines` WHERE `nom` = '$nom' ";
+$num = mysql_query($$sqlchaine);
+
+goto zap ;
+}
+
 
 else 
 {
-$cmd=$_GET["cmd"];
-//$num=str_split($cmd);
-shell_exec("adb shell input text '".$cmd."' ");
+$num=$_GET["cmd"];
 }
 
+zap:
+echo $num ;
+shell_exec("adb shell input text '".$num."' ");
 shell_exec('adb shell exit');
-?>
 
+?>
