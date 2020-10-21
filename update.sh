a0f0b9ef3316fd8d04dc853deeb1c68f9ea70ec9
grillead <adrien.grillet@gmail.com>
2020-10-22 00:52:39 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 {
-rm -Rf freeboxandroidhomecontrol-main.zip
+rm -Rf freeboxandroidhomecontrol-main
 wget -O freeboxandroidhomecontrol.zip https://github.com/grillead/freeboxandroidhomecontrol/archive/main.zip
-unzip freeboxandroidhomecontrol.zip
+unzip -o freeboxandroidhomecontrol.zip
 mv -f freeboxandroidhomecontrol-main/freebox.php /var/www/html/
+rm -f update.sh
 mysqladmin -ufreebox -pfreebox drop chaine -f
 mysqladmin -ufreebox -pfreebox create chaine
 mysql chaine -ufreebox -pfreebox < freeboxandroidhomecontrol-main/chaines.sql -f
 rm -f freeboxandroidhomecontrol.zip
-exit && mv -f freeboxandroidhomecontrol-main/update.sh /home/freebox/ 
+mv -f freeboxandroidhomecontrol-main/update.sh /home/freebox/ 
 }
-
-
