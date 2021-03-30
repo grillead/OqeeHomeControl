638e749f5070c9af12877ef9d3321755e9e49e8b
grillead <adrien.grillet@gmail.com>
2021-03-31 01:34:17 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -6,9 +6,11 @@ unzip -o freeboxandroidhomecontrol.zip
 chmod +x freeboxandroidhomecontrol-main/update.sh
 mv -f freeboxandroidhomecontrol-main/freebox.php /var/www/html/
 rm -f update.sh
-mysqladmin -ufreebox -pfreebox drop chaine -f
-mysqladmin -ufreebox -pfreebox create chaine
-mysql chaine -ufreebox -pfreebox < freeboxandroidhomecontrol-main/chaines.sql -f
+echo 'AddDefaultCharset UTF-8' >> /etc/apache2/conf-enabled/charset.conf
+service apache2 restart
+#mysqladmin -ufreebox -pfreebox drop chaine -f
+#mysqladmin -ufreebox -pfreebox create chaine
+#mysql chaine -ufreebox -pfreebox < freeboxandroidhomecontrol-main/chaines.sql -f
 rm -f freeboxandroidhomecontrol.zip
 mv -f freeboxandroidhomecontrol-main/update.sh /home/freebox/
 rm -Rf freeboxandroidhomecontrol-main
