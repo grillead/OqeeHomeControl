f4ea8b1d5d1d8e6d540ffcf2e65efe82dce348f5
Rémy JUTTIN <remy.juttin@epfl.ch>
2021-04-18 18:29:57 +0200

    Update to PDO to access the DB

--- config.php
+++ config.php
@@ -1,5 +1,20 @@
 <?php
-$setDevice="ip_player";
-$setPort="port_adb_player"; //default 5555
-$conn = mysql_connect("ip_bdd:port_bdd", "user_bdd", "pwd_bdd") or die; 
-$db = mysql_select_db("nom_bdd");
+
+$setDevice="xxx.xxx.xxx.xxx"; //IP player
+$setPort="xxxx"; //Port ADB Player - Default 5555
+
+$hostname = "localhost"; // Default localhost
+$numPort = "3306"; //Port DB - default 3306
+$username = "UsernameDB"; //Username to access the DB
+$password = "PasswordDB"; //Passsword to access the DB
+$dbname = "chaines";//Name of the DB (chaines.sql -> Name : "chaines")
+$dbh =null;
+
+try {
+    $dbh = new PDO("mysql:host=$hostname;dbname=$dbname;port=$numPort", $username, $password);
+}
+catch(PDOException $e)
+{
+	echo 'Connection failed: ' . $e->getMessage();
+}
+
