9abe6f82443ee87f1065e4d8e0fe39b2f484c90a
grillead <adrien.grillet@gmail.com>
2021-04-19 22:29:33 +0200

    Create update2.sh

---
+++ update2.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#Auto update script
+{
+chmod +x freeboxandroidhomecontrol-main/update.sh
+mv -f /freeboxandroidhomecontrol-main/freebox.php /var/www/html/
+rm -f /update.sh
+if grep -Fxq 'AddDefaultCharset UTF-8' /etc/apache2/conf-enabled/charset.conf
+then 
+echo $
+else
+echo 'AddDefaultCharset UTF-8' >> /etc/apache2/conf-enabled/charset.conf
+fi
+service apache2 restart
+rm -f /freeboxandroidhomecontrol.zip
+mv -f /freeboxandroidhomecontrol-main/update.sh /home/freebox/
+rm -Rf /freeboxandroidhomecontrol-main
+exit
+}
