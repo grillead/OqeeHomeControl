3f5dabdc6a2bad31838f9f5a5520741122c23d45
GRILLEAD <adrien.grillet@gmail.com>
2021-04-21 23:55:50 +0200

    Update config.php
    Menage suite passage api freebox

--- config.php
+++ config.php
@@ -1,5 +1,3 @@
 <?php
 $setDevice="ip_player";
 $setPort="port_adb_player"; //default 5555
-$conn = mysql_connect("ip_bdd:port_bdd", "user_bdd", "pwd_bdd") or die; 
-$db = mysql_select_db("nom_bdd");
