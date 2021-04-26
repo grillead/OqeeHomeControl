f9e27ea0fe38f5d5824c1a2bb0d05d3cfa7d46d2
GRILLEAD <adrien.grillet@gmail.com>
2021-04-26 19:02:38 +0200

    Revert "Update freebox.php" (#5)

--- freebox.php
+++ freebox.php
@@ -5,53 +5,35 @@ $_GET["cmd"] = isset($_GET['cmd']) ? $_GET['cmd'] : null ;
 $_GET["nom"] = isset($_GET['nom']) ? $_GET['nom'] : null ;
 $nombrut = isset($_GET['nom']) ? $_GET['nom'] : null ;
 $cmd = isset($_GET['cmd']) ? $_GET['cmd'] : null ;
-$count == 0 ;
-$debug = false;
-$displayinfos = true;
-
-if ($debug == true)
-{
-	ini_set('display_errors', 1);
-	ini_set('display_startup_errors', 1);
-	error_reporting(E_ALL);
-}
 
 
-//Connexion
+//Connection au player
 shell_exec("adb connect '".$setDevice."':'".$setPort."'");
 usleep(800000);
 
-// Var for launching apps :
+//echo $nom ;
+//----------------------------------------------------------------
+// Variable pour lancement d appli :
+
 if ($_GET["cmd"] == "mycanal") 
 {
 shell_exec('adb shell monkey -p com.canal.android.canal 1');
 break;
 }
+//----------------------------------------------------------------
 
-
-echo "<h1><div align=center> Freebox Player POP Remote Control</div> </h1>";
-if ($nombrut != NULL) 
+//Verif si commande avec numeros ou nom de chaine
+if ($_GET["cmd"] != NULL)
 {
-if (strpos($nombrut, 'sur ') !== false) {
-$nombrut = str_replace("sur ", "", $nombrut );
-}
-
-//Check Is channel numbers or channel name
-if (is_numeric($nombrut))
-{
-$num = $nombrut;
-if ($displayinfos == true)
-	{
-		echo "<strong> Change via NUM </strong>";
-		echo '<br/><br/>';
-		echo "Channel number : ".$num;
-		echo '<br/><br/>';
-	}
+$num=$_GET["cmd"];
+echo $cmd;	
 goto zap ;
 }
 
-//Channel rewrite
+if ($nombrut != NULL) 
+{
 
+//réecriture chaine
 if (strpos($nombrut, 'musique') !== false) {
 $nombrut = str_replace("musique", "Music", $nombrut );
 }
@@ -61,56 +43,50 @@ $nombrut = str_replace("plus 1", "+1", $nombrut );
 if (strpos($nombrut, 'manga') !== false) {
 $nombrut = str_replace("manga", "mangas", $nombrut );
 }
+if (strpos($nombrut, 'sur ') !== false) {
+$nombrut = str_replace("sur ", "", $nombrut );
+}
+ //req chaine<>numeros convertie les espace %20 en %
+echo $nombrut;
+echo '</br>';
+//$nombrut = str_replace(" ", "", $nombrut );
+//echo $nombrut;
+//echo '</br>';
 
-//Find channel uuid
 retry:
-if ($count == 1) { $count = 2 ; }
-
 $json1 = file_get_contents('http://mafreebox.freebox.fr/api/v3/tv/channels');
 $data1 = json_decode($json1);
 foreach ($data1->result as $item1) {
+//    if (stripos($item1->name,$nombrut) !==false && $item1->available == 'true') {
 	if (strcasecmp($item1->name, $nombrut) == 0 ){
 	    $uuid = $item1->uuid;
+echo $uuid;
 	echo '</br>';
 goto suite;
  }   
 }
 
 $nombrut = str_replace(" ", "", $nombrut );
-if ($count == 2) { 
-echo "<strong> Channel not found </strong>";
-break ; 
-}
-
-$count = 1;
-
+echo $nombrut;
+echo '</br>';
 goto retry; 
 
-//Find channel number with UUID
+
+
 suite:
 $json2 = file_get_contents('http://mafreebox.freebox.fr/api/v3/tv/bouquets/freeboxtv/channels');
 $data2 = json_decode($json2);
 foreach ($data2->result as $item2) {
     if ($item2->uuid == $uuid) {
-//		echo $item2->number;
+		echo $item2->number;
 		$num = $item2->number;
-//		echo '</br>';
+		echo '</br>';
     }
 }
-
-if ($displayinfos == true)
-	{
-		echo "<strong> Change via NOM </strong>";
-		echo '<br/><br/>';
-		echo "Raw channel number : ".$nombrut;
-		echo '<br/><br/>';
-		echo "Channel number : ".$uuid;
-		echo '<br/><br/>';
-		echo "Channel: ".$num;
-		echo '<br/><br/>';		
-	}
+echo $nom;
 goto zap ;
 }
+
 
 // Changement chaine sur le player
 zap:
